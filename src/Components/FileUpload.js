6f55b51fbb88069529eb66d6320d6a7226e47e49
Yashaswi Sahu <yashaswikrsahu@gmail.com>
2024-07-14 19:43:31 +0530

    commit 3

--- src/Components/FileUpload.js
+++ src/Components/FileUpload.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { useRef } from 'react';
 import axios from "axios";
+import { useNavigate } from 'react-router-dom';
+import Result from '../pages/Result';
 
   const FileUpload=()=>{
     const fileInputRef = useRef(null);
+    const navigate = useNavigate();
+
     const handleFileUpload=()=>{
       fileInputRef.current.click();
     };
@@ -11,10 +15,10 @@ import axios from "axios";
       const file=event.target.files[0];
       
       const formData=new FormData();
-      formData.append("files",file);
+      formData.append("file",file);
 
       // make a POST request to the File Upload API with the FormData object and Rapid API headers
-      axios.post("https://file-upload8.p.rapidapi.com/upload",formData,{
+      axios.post("http://192.168.121.219:3000/predict",formData,{
         headers:{
           "Content-Type":"multipart/form-data",
           // "x-rapidapi-host":"file-upload8.p.rapidapi.com",
@@ -22,12 +26,22 @@ import axios from "axios";
         },
       })
       .then((response)=>{
+        const data = response.data.predictedData;
         //handle the response
-        console.log(response);
+        console.log("Response Received:",response);
+        console.log('Navigating to result with data:', data);
+        //navigate(<Result />);
+        navigate('/result',{state: {data} });
       })
       .catch((error)=>{
-        console.log(error);
+        console.log("Error occurred:",error);
+        const data="Error occurred. Unable to fetch predicted data.";
+        console.log('Navigating to result with error data:', data);
+        navigate('/result',{state: {data} });
       });
+      //.finally(()=>{
+      //  navigate('/result',{state: {data} });
+      //});
     };
   
     return (
